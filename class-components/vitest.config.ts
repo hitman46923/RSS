663dc5d8e1944e2efb0e1c5930858f8c55651951
hitman46923 <al46923@outlook.com>
2025-08-02 21:39:17 +0300

    feat: add  tests , add test to component

--- class-components/vitest.config.ts
+++ class-components/vitest.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from 'vitest/config';
 
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-    plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts',
+  test: {
+    environment: 'jsdom',              
+    setupFiles: './src/setupTests.ts', 
+    globals: true, 
+    coverage: {
+      enabled: true,
+      provider: 'istanbul',
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+      reporter: ['text', 'html'],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
     },
-  });
+  },
+});
